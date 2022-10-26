00947154fe7d50d623f792e41ef48d9706d25974
zzangzwh1 <116697810+zzangzwh1@users.noreply.github.com>
2022-10-25 20:52:37 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace ICA08_MyStruct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.UI_lbl_Result = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // UI_lbl_Result
+            // 
+            this.UI_lbl_Result.AutoSize = true;
+            this.UI_lbl_Result.Location = new System.Drawing.Point(277, 197);
+            this.UI_lbl_Result.Name = "UI_lbl_Result";
+            this.UI_lbl_Result.Size = new System.Drawing.Size(51, 20);
+            this.UI_lbl_Result.TabIndex = 0;
+            this.UI_lbl_Result.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UI_lbl_Result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UI_lbl_Result;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace ICA08_MyStruct
+{
+    public partial class Form1 : Form
+    {
+        private enum eState { State_Idle, State_Armed };
+        static private Random _rnd = new Random();
+        eState type;
+        Point start;
+        Queue<SLine> q = new Queue<SLine>();
+        struct SLine
+        {
+            public Point p;
+            public Color c;
+            public byte thikness;
+            
+            public SLine(Point p, Color c, byte thikness)
+            {
+               this.p = p;
+               this.c = c;
+               this.thikness = thikness;
+            }
+
+        }
+      
+             CDrawer _drawer = new CDrawer(800,800);
+             List<SLine> _lines = new List<SLine>();
+        public Form1()
+        {
+            InitializeComponent();
+       
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+      
+            switch (type) 
+            {
+                case eState.State_Idle:
+                    UI_lbl_Result.Text = eState.State_Idle.ToString();
+                        if(_drawer.GetLastMouseLeftClick(out start))
+                        {
+                        type = eState.State_Armed;
+                        }
+                    break;
+                case eState.State_Armed:
+                   SLine sl = new SLine();
+                    UI_lbl_Result.Text = eState.State_Armed.ToString();
+                    if (_drawer.GetLastMouseLeftClick(out sl.p))
+                    {
+                        sl.c = Color.Red;
+                        sl.thikness = 5;
+
+                        Render(sl);
+                        type = eState.State_Idle;
+                        _lines.Add(sl);
+                    }
+                
+                
+                        break;
+
+            }
+            Point temp;
+            if (_drawer.GetLastMouseRightClick(out temp))
+            {
+
+                for (int i = 1; i < _lines.Count; i++)
+                {
+                    SLine slNew = new SLine();
+                    slNew = _lines[i];
+                    slNew.c = RandColor.GetColor();
+                    slNew.thikness = (byte)_rnd.Next(1, 16);
+                    q.Enqueue(slNew);
+
+
+                }
+                Console.WriteLine(q.Count);
+                RenderRight();
+
+
+            }
+
+
+
+
+
+
+
+        }
+        private void Render(SLine li)
+        {
+            _drawer.AddLine(start.X,start.Y, li.p.X, li.p.Y,li.c, li.thikness);
+        }
+       private void RenderRight()
+        {
+            _drawer.Clear();
+            for(int i =0; i <_lines.Count; i++)
+            {
+              SLine slNew  = q.Dequeue();
+                _drawer.AddLine(start.X,start.Y,slNew.p.X,slNew.p.Y,slNew.c,slNew.thikness);
+            }
+            
+
+        }
+
+    }
+}
